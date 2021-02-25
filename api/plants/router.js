ed80f4360ad663d8789e0ffc0cc7433020b1a9f0
harvey magana <heyharv2012@gmail.com>
2021-02-24 20:09:26 -0800

    Updated the get by id method in the plants router file.

--- api/plants/router.js
+++ api/plants/router.js
@@ -16,18 +16,14 @@ router.get("/", (req, res) => {
 //put (id)
 //delete (id)
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   const { id } = req.params;
-
-  db('plants').where({ id }).first()
-      .then(plant => {
-          res.json(plant);
-      })
-      .catch(err => {
-          res.status(500).json({
-              message: 'Failed to retrieve plant by id.'
-          })
-      })
+  try {
+    const data = await db.getById(id)
+    res.status(200).json(data);
+  } catch(err) {
+      next(err);
+  }
 })
 
 router.post('/', (req, res) => {
@@ -48,7 +44,23 @@ router.post('/', (req, res) => {
 })
 
 router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
 
+  Schemes.findById(id)
+    .then(scheme => {
+      if (scheme) {
+        return Schemes.update(changes, id);
+      } else {
+        res.status(404).json({ message: 'Could not find scheme with given id' });
+      }
+    })
+    .then(updatedScheme => {
+      res.json(updatedScheme);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to update scheme' });
+    });
 })
 
 router.delete('/:id', (req, res) => {
