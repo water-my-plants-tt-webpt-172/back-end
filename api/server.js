f66d2c2b394343229a3f0d9f0cd665f489ad2f10
harvey magana <heyharv2012@gmail.com>
2021-02-24 20:31:47 -0800

    Removed commented code from the auth router and /auth from many of the routes in the server file.

--- api/server.js
+++ api/server.js
@@ -9,9 +9,9 @@ const authRouter = require("./auth/router");
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
-server.use("/api/plants", plantsRouter);
-server.use("/api/users", usersRouter);
-server.use("/api/auth", authRouter);
+server.use("/plants", plantsRouter);
+server.use("/users", usersRouter);
+server.use("/auth", authRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "server up" });
