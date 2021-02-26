59121ff39bcf824d6097bf447fda5e517ff7b3e1
harvey magana <heyharv2012@gmail.com>
2021-02-25 18:59:52 -0800

    Updated the migration file so that includes the schema for plants.

--- data/migrations/20210224205907_create-plants.js
+++ data/migrations/20210224205907_create-plants.js
@@ -1,8 +1,19 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable('plants', tbl => {
+        tbl.increments();
+        tbl.string('nickname', 128)
+            .notNullable();
+        tbl.string('species', 128)
+            .notNullable();
+        tbl.integer('h2oFrequency')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE');
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema
+    .dropTableIfExists('plants');
 };
