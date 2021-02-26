c4290f75c315bdb5c4a778603a2c65726ca2c1c0
harvey magana <heyharv2012@gmail.com>
2021-02-25 19:14:34 -0800

    Created the seeds file for the plants.

--- data/seeds/001-plants.js.js
+++ data/seeds/001-plants.js.js
@@ -1,13 +1,28 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('plants').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('plants').insert([
+        { 
+          id: 1,
+          nickname: 'Devil\'s ivy' , 
+          species: 'Epipremnum quorum',
+          h2oFrequency: 3
+        },
+        { 
+          id: 2,
+          nickname: 'Angel Wings Cactus' , 
+          species: 'Opunta albispina',
+          h2oFrequency: 1
+        },
+        { 
+          id: 3,
+          nickname: 'Chocolate Soldier' , 
+          species: 'Kalanchoe tomentosa',
+          h2oFrequency: 3
+        }
       ]);
     });
 };
