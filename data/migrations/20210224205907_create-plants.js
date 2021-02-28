917ffe883357f452a6e352d4f818ef6eeaa5f965
harvey magana <heyharv2012@gmail.com>
2021-02-28 10:17:28 -0800

    Updated the plants schema, trying to resolve the conflict in the main branch.

--- data/migrations/20210224205907_create-plants.js
+++ data/migrations/20210224205907_create-plants.js
@@ -1,19 +1,18 @@
 
 exports.up = function(knex) {
-  return knex.schema
-    .createTable('plants', tbl => {
-        tbl.increments();
-        tbl.string('nickname', 128)
-            .notNullable();
-        tbl.string('species', 128)
-            .notNullable();
-        tbl.integer('h2oFrequency')
-            .onDelete('CASCADE')
-            .onUpdate('CASCADE');
-        tbl.string('image_url');
-        tbl.date('isWatered');
-    })
-};
+    return knex.schema
+      .createTable('plants', tbl => {
+          tbl.increments();
+          tbl.string('nickname', 128)
+              .notNullable();
+          tbl.string('species', 128)
+              .notNullable();
+          tbl.integer('h2oFrequency')
+              .notNullable();
+          tbl.string('image_url');
+          tbl.date('isWatered');
+      })
+  };
 
 exports.down = function(knex) {
     return knex.schema
