fadc6768a38d3b6877bdd2c0ecab000eb6ee0f1e
harvey magana <heyharv2012@gmail.com>
2021-03-02 19:31:04 -0800

    Updated the plants schema so that it has an id.

--- data/migrations/20210224205907_create-plants.js
+++ data/migrations/20210224205907_create-plants.js
@@ -1,7 +1,7 @@
 
 exports.up = function (knex) {
   return knex.schema.createTable("plants", (tbl) => {
-    tbl.increments();
+    tbl.increments('id');
     tbl.string("nickname", 128).notNullable();
     tbl.string("species", 128).notNullable();
     tbl.integer("h2oFrequency").notNullable();
@@ -11,8 +11,8 @@ exports.up = function (knex) {
       .references("users.id")
       .onDelete("CASCADE")
       .onUpdate("CASCADE");
-              tbl.string('image_url');
-          tbl.date('isWatered');
+      tbl.string('image_url');
+      tbl.date('isWatered');
   });
 };
 
