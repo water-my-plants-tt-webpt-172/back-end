a134f28cff9684236ab259204371be30c33b36c0
Erick Villegaz <64494158+evllz@users.noreply.github.com>
2021-03-05 18:29:38 -0800

    Same

--- data/migrations/20210305163450_new-columns.js
+++ data/migrations/20210305163450_new-columns.js
@@ -7,8 +7,5 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-    return knex.schema.table('plants', (tbl) => {
-      tbl.dropColumn("image_url");
-      tbl.dropColumn('isWatered');
-    })
+
 };
